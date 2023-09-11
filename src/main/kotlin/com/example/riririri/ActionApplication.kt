1f4a6542ba553447eb329c195f45c3a45b3863f9
jimmy.lee <dongeol92@daum.net>
2023-09-11 18:26:32 +0900

    init

---
+++ src/main/kotlin/com/example/riririri/ActionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.riririri
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RiriririApplication
+
+fun main(args: Array<String>) {
+    runApplication<RiriririApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/riririri/ActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.riririri
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
