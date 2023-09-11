e2f460630cdd933c7116603a5d6bdb322977ead6
jimmy.lee <dongeol92@daum.net>
2023-09-12 07:14:38 +0900

    docker-compose 구성 완료

--- src/main/kotlin/com/example/riririri/HealthCheckController.kt
+++ src/main/kotlin/com/example/riririri/HealthCheckController.kt
@@ -1,10 +1,14 @@
 package com.example.riririri
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController {
+
+    @Value("\${app.version:1.0.0}")
+    lateinit var version: String
 
     @GetMapping("/health")
     fun healthCheck(): String {
